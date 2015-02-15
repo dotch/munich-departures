26cccb1b2bf353317afa0a53b56c0cc1c77758df
dotch <christian.x.weiss@gmail.com>
2015-02-15 03:49:05 +0100

    packages

---
+++ jspm_packages/npm/buffer@3.0.2/test/node/test-buffer-big.js
@@ -0,0 +1,10 @@
+/* */ 
+(function(process) {
+  var Buffer = require("../../../buffer@3.0.2").Buffer;
+  if (process.env.OBJECT_IMPL)
+    Buffer.TYPED_ARRAY_SUPPORT = false;
+  var assert = require("assert");
+  assert.throws(function() {
+    new Buffer(0x3fffffff + 1);
+  }, RangeError);
+})(require("process"));
---
+++ jspm_packages/npm/buffer@3.0.2/test/node/test-buffer-slice.js
@@ -0,0 +1,11 @@
+/* */ 
+(function(process) {
+  var Buffer = require("../../../buffer@3.0.2").Buffer;
+  if (process.env.OBJECT_IMPL)
+    Buffer.TYPED_ARRAY_SUPPORT = false;
+  var assert = require("assert");
+  var Buffer = require("../../../buffer@3.0.2").Buffer;
+  var buff = new Buffer(Buffer.poolSize + 1);
+  var slicedBuffer = buff.slice();
+  assert.equal(slicedBuffer.parent, buff, "slicedBufffer should have its parent set to the original " + " buffer");
+})(require("process"));
